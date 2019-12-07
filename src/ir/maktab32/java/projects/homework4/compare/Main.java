c115f65ea3f59cbb8841c8c882d8c947e2887ce8
arfjava <arrfjava@gmail.com>
2019-12-07 06:50:29 +0330

    class Main completed.

--- src/ir/maktab32/java/projects/homework4/compare/Main.java
+++ src/ir/maktab32/java/projects/homework4/compare/Main.java
@@ -1,8 +1,30 @@
 package ir.maktab32.java.projects.homework4.compare;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        List<Car> cars = new ArrayList<Car>();
+        cars.add(new Car("Benz", "Blue", 2014));
+        cars.add(new Car("Toyota", "Black",2010));
+        cars.add(new Car("Hyundai", "Silver", 2018));
+
+        Collections.sort(cars);
+        System.out.println("Sorted by Model:");
+        for (Car car : cars)
+            System.out.println(car);
+
+        System.out.println();
+
+        CompareTitles compareTitles = new CompareTitles();
+
+        Collections.sort(cars, compareTitles);
+        System.out.println("Sorted by Title:");
+        for (Car car : cars)
+            System.out.println(car);
     }
 }
